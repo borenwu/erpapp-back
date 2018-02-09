edaf942680b9b445c71c0bbbbd5585a1de83069c
borenwu <borenwuapple@163.com>
2018-02-09 16:09:58 +0800

    new

--- api/services/checkService.js
+++ api/services/checkService.js
@@ -1,11 +1,18 @@
 const CheckService = {
 
   checkCompanyName: function(companyName) {
+
+        // Something.findOne(criteria).exec(function (err, record) {
+
+        // });
+
         return new Promise((resolve, reject) => {
             Company.findOne({ company_name: companyName })
-                .then(_company => {
-                    if (!_company) reject(new Error('Unable to find company record'))
-                    else resolve(_company)
+                .then((_company,err) => {
+                    if (err) reject(err)
+                    if (!_company) reject(new Error('Unable to find client record'))
+                    console.log(_company)
+                    resolve(_company)
                 })
         })
   },
@@ -13,7 +20,8 @@ const CheckService = {
   checkClientName:function (company_id,clientName) {
     return new Promise((resolve,reject)=>{
       Client.findOne({client_name:clientName,company_id:company_id})
-        .then(_client =>{
+        .then((_client,err) =>{
+          if (err) reject(err)
           if (!_client) reject(new Error('Unable to find client record'))
           else resolve(_client)
         })
